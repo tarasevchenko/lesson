3f7e3dbe187e0e5200f641895c71d9e9d4366592
tarasevchenko <taras.evchenko@gmail.com>
2017-09-18 22:00:24 +0400

    vercoreDataVer2

--- adviceWithCoreData/ViewControllerFavoritesAdvice.swift
+++ adviceWithCoreData/ViewControllerFavoritesAdvice.swift
@@ -10,32 +10,32 @@ import Foundation
 import UIKit
 import CoreData
 class   ViewControllerThree : UIViewController, UITableViewDelegate,UITableViewDataSource {
-   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (adviceShare.sharedAdvice.share.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let adviceSaves = adviceShare.sharedAdvice.share[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "adviceCell", for: indexPath)
+        //UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "adviceCell")
+        
+        cell.textLabel?.text = adviceSaves.value(forKeyPath: "advicesText") as? String
+        
+        return cell
+        
+        
+        
+        
+    }
     @IBOutlet weak var tableView: UITableView!
     
    
     
    
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (adviceShare.sharedAdvice.share.count)
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-
-        let adviceSaves = adviceShare.sharedAdvice.share[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "adviceCell", for: indexPath)
-            //UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "adviceCell")
-       
-        cell.textLabel?.text = adviceSaves.value(forKeyPath: "advicesText") as? String
-        
-        return cell
-
-        
    
-   
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -45,12 +45,25 @@ class   ViewControllerThree : UIViewController, UITableViewDelegate,UITableViewD
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+      let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "AdviceStorage")
+        do {
+            adviceShare.sharedAdvice.share = try managedContext.fetch(fetchRequest)
+        }
+        catch let error as NSError {
+            print ("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
         tableView.reloadData()
     }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         
     }
